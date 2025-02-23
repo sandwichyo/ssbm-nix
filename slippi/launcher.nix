6aa5676ac0afe8a1a5fdd499ebee3b50d81e96b5
James Kan <jimmy.kan1990@gmail.com>
2025-02-23 11:26:17 +0100

    Update Launcher

--- slippi/launcher.nix
+++ slippi/launcher.nix
@@ -9,11 +9,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "slippi-launcher";
-  version = "2.11.6";
+  version = "2.11.8";
 
   src = fetchurl {
     url = "https://github.com/project-slippi/slippi-launcher/releases/download/v${version}/Slippi-Launcher-${version}-x86_64.AppImage";
-    hash = "sha256-pdBPCQ0GL7TFM5o48noc6Tovmeq+f2M3wpallems8aE=";
+    hash = "sha256-8krtzo878nh6oMTUhyPJIa/RkkebqQckb4Mh2+9SaiU=";
   };
   dontUnpack = true;
 
